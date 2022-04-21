0cf07cbd1832254b1d85afed51e81132a25172f9
Ahmed Mazin <lemon@lemondev.xyz>
2022-04-22 00:23:10 +0300

    96_microsoft_list_permutation

---
+++ 96_microsoft_list_permutation/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func exp(x int) int {
+	if x <= 1 { return 1 }
+	return x * exp(x-1)
+}
+
+func solve(s []int) [][]int {
+	ret := append([][]int{}, s)
+	i, j := 1, 2
+	slice := append([]int{}, s...)
+	for x := 0; x < exp(len(s))-1; x++ {
+		slice[i], slice[j] = slice[j], slice[i]
+		ret = append(ret, append([]int{}, slice...))
+
+		i++
+		j++
+		
+		if j >= len(s) { i, j = 0, 1 }
+	}
+	
+	return ret
+}
+
+func main() {
+	fmt.Println(solve([]int{1, 2, 3}))
+}
