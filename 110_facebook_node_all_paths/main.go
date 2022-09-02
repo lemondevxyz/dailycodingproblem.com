d234b25f85eb98df3ff49e96e53fc6cbbb805ad8
Ahmed Mazin <lemon@lemondev.xyz>
2022-09-02 12:32:19 +0300

    fix 110_facebook_node_all_paths

--- 110_facebook_node_all_paths/main.go
+++ 110_facebook_node_all_paths/main.go
@@ -8,38 +8,61 @@ type Node struct {
 	Right *Node
 }
 
-func (n *Node) Do(arr [][]int, index int) [][]int {
-	addIndex := false
-	if n.Left != nil && n.Right != nil {
-		addIndex = true
-	}
-
+func (n *Node) Do(arr [][]int, index int) ([][]int, int) {
+	original := append([]int{}, arr[index]...)
 	if n.Left != nil {
-		arr[index] = append(arr[index], n.Left.Val)
-		n.Left.Do(arr, index)
+		arr[index] = append(append([]int{}, arr[index]...), n.Left.Val)
+		arr, index = n.Left.Do(arr, index)
 	}
 
-	if addIndex {
-		slice := arr[index]
-		if len(slice) != 0 {
-			slice = slice[:len(slice)-1]
-		}
-
-		arr = append(arr, append([]int{}, slice...))
+	if n.Left != nil && n.Right != nil {
+		arr = append(arr, append([]int{}, original...))
 		index++
 	}
 
 	if n.Right != nil {
-		arr[index] = append(arr[index], n.Right.Val)
-		arr = n.Right.Do(arr, index)
+		arr[index] = append(append([]int{}, original...), n.Right.Val)
+		arr, index = n.Right.Do(arr, index)
 	}
 
-	return arr
+	return arr, index
 }
 
 func main() {
-	n := &Node{1, &Node{2, nil, nil}, &Node{3, &Node{4, nil, nil}, &Node{5, nil, nil}}}
+	n := &Node{
+		1,
+		&Node{
+			2,
+			&Node{
+				4,
+				&Node{
+					5, nil, nil,
+				},
+				nil,
+			},
+			&Node{
+				3, nil, nil,
+			},
+		},
+		&Node{
+			6,
+			nil,
+			&Node{
+				7,
+				&Node{
+					8,
+					&Node{
+						10, nil, nil,
+					},
+					&Node{
+						9, nil, nil,
+					},
+				},
+				nil,
+			},
+		},
+	}
 
-	arr := n.Do([][]int{{n.Val}}, 0)
+	arr, _ := n.Do([][]int{{n.Val}}, 0)
 	fmt.Println(arr)
 }
